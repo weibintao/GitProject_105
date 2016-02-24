12683806961b241ea461833f8c730230966374bf
w_326 <1047912930@qq.com>
2016-02-24 14:47:04 +0800

    new
    三点水

--- Test_gitPrj/Test_gitPrj/ViewController.m
+++ Test_gitPrj/Test_gitPrj/ViewController.m
@@ -18,6 +18,10 @@
     [super viewDidLoad];
     self.title = @"github 代码修改";
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
 - (void)didReceiveMemoryWarning {
